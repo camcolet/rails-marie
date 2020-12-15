98f4383a5e61bfe1d7342dae9de8fd7e029ac8b5
camille colet <camcolet@gmail.com>
2020-12-15 16:42:30 +0100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,3 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_101258) do
-
-  create_table "mailers", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-end
